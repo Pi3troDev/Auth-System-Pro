77872e63c39247659a6c7fb711b359d401c8ef50
Pietro Alves <pietroalves.dev@gmail.com>
2026-04-04 10:57:18 -0300

    remoção de arquivos desnecessários

--- packages/shared/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { User, CreateUserRequest, CreateUserResponse } from './types/User';
-export { ApiResponse, ApiError, PaginationParams, PaginatedResponse } from './types/Response';
-export {
-  ApplicationError,
-  ValidationError,
-  NotFoundError,
-  UnauthorizedError,
-} from './errors';
